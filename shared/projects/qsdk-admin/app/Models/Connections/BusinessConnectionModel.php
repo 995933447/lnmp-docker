c11f7dbe7809769b847712b3ec0e950b42ee2f82
yzh <995933447@qq.com>
2020-11-30 16:32:53 +0800

    newest

--- shared/projects/qsdk-admin/app/Models/Connections/BusinessConnectionModel.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Models\Connections;
-
-use Illuminate\Database\Eloquent\Model;
-
-class BusinessConnectionModel extends Model
-{
-    protected $connection = 'mysql_business';
-}
--- shared/projects/qsdk-admin/app/Models/Connections/StatisticsConnectionModel.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Models\Connections;
-
-use Illuminate\Database\Eloquent\Model;
-
-class StatisticsConnectionModel extends Model
-{
-    protected $connection = 'mysql_statistics';
-}
